28ef2bf546697aa83fc5a113de71c8cd5a0f6b59
VladislavPenchev <vvld964@gmail.com>
2019-01-27 13:20:05 +0200

    Android_CODE

---
+++ Android_LED/app/src/main/java/penchev/vladislav/myapplication/MainActivity.java
@@ -0,0 +1,65 @@
+package penchev.vladislav.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.gson.JsonObject;
+
+import penchev.vladislav.myapplication.retrofit.ApiCallInterface;
+import penchev.vladislav.myapplication.retrofit.RetrofitUtil;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button off = findViewById(R.id.off);
+        Button on = findViewById(R.id.on);
+
+        final ApiCallInterface apiCallInterface = RetrofitUtil.getApiCallInterface();
+
+        on.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                apiCallInterface.on().enqueue(new Callback<JsonObject>() {
+                    @Override
+                    public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
+                        Toast.makeText(MainActivity.this, "Successful on", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onFailure(Call<JsonObject> call, Throwable t) {
+                        Toast.makeText(MainActivity.this, "Fail on", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+
+        off.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                apiCallInterface.off().enqueue(new Callback<JsonObject>() {
+                    @Override
+                    public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
+                        Toast.makeText(MainActivity.this, "Successful off", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onFailure(Call<JsonObject> call, Throwable t) {
+                        Toast.makeText(MainActivity.this, "Fail off", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+        
+        
+    }
+}
---
+++ Android_LED/app/src/main/java/penchev/vladislav/myapplication/retrofit/ApiCallInterface.java
@@ -0,0 +1,19 @@
+package penchev.vladislav.myapplication.retrofit;
+
+import com.google.gson.JsonObject;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface ApiCallInterface {
+
+    // Base url
+    String BASE_URL = "http://192.168.1.31/5/";
+
+    @GET("off")
+    Call<JsonObject> off();
+
+    @GET("on")
+    Call<JsonObject> on();
+
+}
